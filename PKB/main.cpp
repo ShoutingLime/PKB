f32543c2fb1b96fb75894cc208933e14988682f6
ShoutingLime <shouting.lime@gmail.com>
2018-06-07 23:00:27 +0300

    Project with only jpeg image

--- PKB/main.cpp
+++ PKB/main.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtWidgets>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
+    QLabel lbl;
+    lbl.setPixmap(QPixmap("d:/Work/PKB/PKB/img/avatar.jpeg"));
+    lbl.show();
     return a.exec();
 }
